aca9e91ea2661ff76a35f0621047876711fd6d77
EdoardoPanzeri1 <edoardopanzeriwork@gmail.com>
2024-07-18 16:05:05 +0200

    create main.go and add basic functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/EdoardoPanzeri1/mangabox
 
 go 1.22.3
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/EdoardoPanzeri1/mangabox/internal/database"
+	"github.com/joho/godotenv"
+)
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
+func main() {
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port environment variable is not set")
+	}
+
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		log.Fatal("DATABASE_URL environment is not set")
+	}
+
+	db, err := sql.Open("postegres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
+
+	mux := http.NewServeMux()
+
+	// put handle func under here
+	mux.HandleFunc("GET /v1/healtz", handlerReadiness)
+	mux.HandleFunc("GET /v1/err", handlerErr)
+
+	srv := &http.Server{
+		Addr:              ":" + port,
+		Handler:           mux,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+
+	log.Printf("Serving file on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
